50169f58ee32008e5dea3a9902275c7245e61244
SULAIMAN SHO <sulaiman.lutfi.sho@gmail.com>
2026-07-29 23:57:55 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,93 @@
+
+function clock() {
+    const d = new Date();
+    time.innerHTML = d.toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit'
+    });
+    date.innerHTML = `${d.getFullYear()}/${d.getMonth() + 1}/${d.getDate()}`;
+}
+clock();
+setInterval(clock, 1000);
+
+// Canvas Background
+const c = document.getElementById("bg");
+const ctx = c.getContext("2d");
+let w, h, p = [];
+function resize() {
+    w = c.width = innerWidth;
+    h = c.height = innerHeight;
+}
+addEventListener("resize", resize);
+resize();
+
+for (let i = 0; i < 180; i++) {
+    p.push({
+        x: Math.random() * w,
+        y: Math.random() * h,
+        r: Math.random() * 2 + 1,
+        vx: (Math.random() - .5) * .15,
+        vy: (Math.random() - .5) * .15
+    });
+}
+
+let t = 0;
+function draw() {
+    t += 0.002;
+    ctx.clearRect(0, 0, w, h);
+
+    let g = ctx.createRadialGradient(
+        w / 2 + Math.cos(t) * 250,
+        h / 2 + Math.sin(t * .8) * 180,
+        80,
+        w / 2, h / 2,
+        900
+    );
+    g.addColorStop(0, "rgba(0,170,255,.30)");
+    g.addColorStop(.5, "rgba(0,80,255,.10)");
+    g.addColorStop(1, "rgba(0,0,0,0)");
+    ctx.fillStyle = g;
+    ctx.fillRect(0, 0, w, h);
+
+    for (const a of p) {
+        a.x += a.vx;
+        a.y += a.vy;
+        if (a.x < 0) a.x = w;
+        if (a.x > w) a.x = 0;
+        if (a.y < 0) a.y = h;
+        if (a.y > h) a.y = 0;
+
+        ctx.beginPath();
+        ctx.arc(a.x, a.y, a.r, 0, Math.PI * 2);
+        ctx.fillStyle = "rgba(120,210,255,.85)";
+        ctx.fill();
+    }
+    requestAnimationFrame(draw);
+}
+draw();
+
+
+
+
+
+
+
+
+
+const toggle = document.getElementById("toggleApps");
+
+const apps = document.getElementById("googleApps");
+
+const arrow = document.getElementById("arrow");
+
+toggle.onclick = () => {
+
+    apps.classList.toggle("show");
+
+    arrow.innerHTML = apps.classList.contains("show")
+
+        ? "▲"
+
+        : "▼";
+
+}
